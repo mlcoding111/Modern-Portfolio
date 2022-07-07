a0228eb94ee311e9c3924727a888b39cf1703a0f
mlcoding111 <kennycodes@hotmail.com>
2022-07-07 17:09:42 -0400

    Changed header display grid for flex layout

--- src/containers/Header/index.jsx
+++ src/containers/Header/index.jsx
@@ -1,52 +1,62 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import * as Styled from './styles.js'
+
 function Header() {
   return (
     <>
-      <HeaderWrap>
+      <Styled.Header>
 
-        <div className="hero-img">
+        <div className="hero">
 
-        </div>
+          <div className="hero-img"></div>
 
-        <div className="hero-description">
+          <div className="hero-description">
+            <div className="hero-text">
+              <span id="my-name">Hello, im Michael</span>
+              <h1 id="profession">Web Developer</h1>
+              <p className="description">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries,
+              </p>
+            </div>
 
-          <div className="hero-text">
-            <span id="my-name">Hello, im Michael</span>
-            <h1 id="profession">Web Developer</h1>
-            <p className='description'>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of 
-              type and scrambled it to make a type specimen book. It has survived not only five centuries,
-
-            </p>
+            <div className="hero-button">
+              <button>Contact</button>
+              <button>Resume</button>
+            </div>
           </div>
-
-          <div className="hero-button">
-            <button>Contact</button>
-            <button>Resume</button>
-          </div>                    
-
         </div>
 
         <div className="skills-display">
-          <SkillCard>
-            <h3>Front End</h3>
-            <p>Skilled using recent technologies to take any layout to life</p>
-          </SkillCard>
 
-          <SkillCard red>
-            <h3>Web Design</h3>
-            <p>Create design using tools like figma</p>
+            <Styled.SkillCard>
+              <div>
+                <h3>Front End</h3>
+                <p>
+                  Skilled using recent technologies to take any layout to life
+                </p>
+              </div>
+            </Styled.SkillCard>
 
-          </SkillCard>
+            <Styled.SkillCard red>
+              <div>
+                {" "}
+                <h3>Web Design</h3>
+                <p>Create design using tools like figma</p>
+              </div>
+            </Styled.SkillCard>
 
-          <SkillCard>
-            <h3>Back-End</h3>
-            <p>Setup NodeJs server and database structure</p>
-          </SkillCard>
-
+            <Styled.SkillCard>
+              <div>
+                <h3>Back-End</h3>
+                <p>Setup NodeJs server and database structure</p>
+              </div>
+            </Styled.SkillCard>
         </div>
-
+        
         <div className="stats-display">
             <div>
               <span id="year">3</span>
@@ -57,17 +67,11 @@ function Header() {
               <button>My skills</button>
             </div>
         </div>
-      </HeaderWrap>
+
+      </Styled.Header>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
 
-export const HeaderWrap = styled.header`
-  
-`
-
-export const SkillCard = styled.div`  
-  background: ${props => props.red ? props.theme.secondary : "#282828"}
-`
--- src/containers/Header/styles.js
+++ src/containers/Header/styles.js
@@ -1,1 +1,91 @@
-import { styled } from 'styled-components'
+import styled from 'styled-components'
+
+export const Header = styled.header`
+  margin: 0 auto;
+  max-width: 90%;
+  border: 1px solid red;
+  min-height: 85vh;
+  padding: 4em 0;
+
+  
+  .hero{
+    padding: 0 4em;  // Change it for mobile
+    display: flex;
+    flex-wrap: wrap;    
+    /* display: grid;
+    gap: 1em;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); // replace 1fr for auto when image will be uploaded */
+  }
+  
+  .hero > * {
+    border: 1px solid white;
+    padding: 3em; // Change it for mobile
+    flex: 1 1 10em;
+  }
+
+  .hero-description{
+    flex: 2;
+  }
+  
+
+  .hero-button > *{
+    padding: 1em 2em;
+    margin: 3em .5em;
+  }
+
+  .hero-text{
+    padding-top: 5em;
+  }
+
+  .hero-text .description{
+    max-width: 600px;
+  }
+  
+  .hero-text #profession{
+    margin-top: auto;
+    margin-bottom: .5em;
+    font-size: 4rem;
+  }
+
+  .hero-text #my-name{
+    font-size: 1.4rem;
+    color: ${props => props.theme.secondary};
+  }
+
+  .skills-display{
+    position: relative;
+    max-width: 1000px;
+    margin: 3em auto;
+    padding: 2em 0;
+    display: flex;
+    flex-wrap: wrap;
+    /* display: grid;
+    gap: 1em;
+    grid-template-columns: repeat(auto-fit, minmax(250px, 1fr)); */
+  }
+  .skills-display > * {
+    margin: .5em;
+    flex: 1 1 10em;
+    gap: 1em;
+  }
+
+  .stats-display{
+    position: relative;
+    margin: 3rem 0;
+    display: flex;
+    justify-content: space-around;
+    text-align: center;
+    color: ${props => props.theme.secondary};
+  }
+  
+
+  #year{
+    font-size: 5rem;
+  }
+
+`;
+
+export const SkillCard = styled.div`
+  padding: 4em 1em;
+  background: ${(props) => (props.red ? props.theme.secondary : "#282828")};
+`;
