f5bcc577636f9334b2b3c60e11c89d5ee15d931c
mlcoding111 <kennycodes@hotmail.com>
2022-07-07 13:30:33 -0400

    Added Skills content

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Skills() {
   return (
-    <div>Skills</div>
+    <SkillsWrap>
+      <SectionHeader>
+        <h1>Showcase</h1>
+        <span>My skills</span>
+      </SectionHeader>
+
+      <button>Toggle button</button>
+    </SkillsWrap>
   )
 }
 
 export default Skills
+
+export const SectionHeader = styled.div`
+  
+`
+
+export const SkillsWrap = styled.div`
+  
+`
