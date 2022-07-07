aab2913495b3fe853747e3df2e7b6d0a8694e52b
mlcoding111 <kennycodes@hotmail.com>
2022-07-07 11:44:59 -0400

    Added react-router-dom@6 to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import React from "react";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./containers/Home";
+
+import Navigation from './containers/Navigation'
+import Projects from './components/Projects'
+import Footer from './containers/Footer'
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <BrowserRouter>
+      <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/project:id" element={<Projects />} />
+        </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
