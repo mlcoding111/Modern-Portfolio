dcc4ee46c10a610c24b6e07ceb9b17ffad8a41f4
mlcoding111 <kennycodes@hotmail.com>
2022-07-07 14:01:43 -0400

    Updated Navigation basic layout styles

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <FooterWrap>
+      <ul>
+        <li>Michael Lacroix</li>
+        <li>Quebec, Canada</li>
+        <li>819-880-0461</li>
+      </ul>
+
+      <div className="brand">
+        Brand here
+      </div>
+
+      <div>kennycoding111@gmail.com</div>
+    </FooterWrap>
   )
 }
 
 export default Footer
+
+
+export const FooterWrap = styled.footer`
+  
+`
--- src/containers/Navigation/index.jsx
+++ src/containers/Navigation/index.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Navigation() {
   return (
-    <div>Navigation</div>
+    <NavigationWrap>
+      <div className="brand">
+        <svg width="38" height="28" viewBox="0 0 38 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11.3554 27.9454L0 14.0414L11.3501 0.00280762L14.4423 2.49867L5.12101 14.0282L14.4343 25.4311L11.3554 27.9454Z" fill="#D72323"/>
+          <path d="M26.349 27.9453L23.2595 25.4468L32.5781 13.9172L23.2648 2.51434L26.3437 0L37.6991 13.904L26.349 27.9453Z" fill="#D72323"/>
+        </svg>
+        
+        <h1>MLWeb</h1>
+      </div>
+      <PrimaryNav>
+        <li>Home</li>
+        <li>Projects</li>
+        <li>About</li>
+        <li>Contact</li>
+      </PrimaryNav>
+    </NavigationWrap>
   )
 }
 
 export default Navigation
+
+export const NavigationWrap = styled.nav`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background: rgba(0, 0, 0, 0.62);
+
+    .brand{
+      display: flex;
+      align-items: center;
+    }
+    
+    .brand svg{
+      margin-right: .2rem;
+    }
+
+    > * {
+      display: inherit;
+    }
+`
+
+export const PrimaryNav = styled.ul`
+  list-style: none;
+
+  > * {
+    padding: 1rem;
+  }
+
+  > *:hover {
+    padding: 1rem;
+    background: ${props => props.theme.secondary};
+    cursor: pointer;
+  }
+`
