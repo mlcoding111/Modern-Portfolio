ee9d717d3e60214215032aedf46e77fa7bfdcf7b
mlcoding111 <kennycodes@hotmail.com>
2022-07-11 11:11:06 -0400

    Updated Reusable button animation

--- src/components/Reusable/Button/index.jsx
+++ src/components/Reusable/Button/index.jsx
@@ -1,71 +1,101 @@
-import React from 'react'
-import styled from 'styled-components'
-import {motion} from 'framer-motion'
+import React, { useRef } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
 
-function Button() {
-
-    const [hovered, setHovered] = React.useState(false); 
-  return (
-    <Wrap onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-            initial={false}
-            animate={{ scale: hovered ? 1.1 : 1 }}
-    >
-        <motion.a initial={false}
-          animate={{ scale: hovered ? 5.2 : 0 , transition: { duration: 0.5, ease: "easeIn"}}}> 
-        </motion.a>
-{/*         
-        <span>Read more</span> */}
-    </Wrap>
-  )
-}
-
-export default Button
-
-export const Wrap = styled(motion.button)`
-    color: white;
-    background: transparent;
-    border: 1px solid #fff;
-    border-radius: 50%;
-    height: 11rem;
-    width: 11rem;
-    cursor: pointer;
-    padding: 0;
-    z-index: 999;
-    position: relative;
-    user-select: none;
-    z-index: 1;
-    color: black;
-
-    a{
-        border: 1px solid white;
-        display: block;
-        margin: 0 auto;
-        background-color: #fff;
-        content: "";
-        bottom: auto;
-        width: 20%;
-        height: 20%;
-        box-sizing: border-box;
-        z-index: -1;
-        border-radius: 50%;
+function getRelativeCoordinates(event, referenceElement){
+    const position = {
+        x: event.pageX,
+        y: event.pageY
     }
 
-    /* ::after{
-        transform: translate(-50%, -50%);
-        background-color: white;
-        content: "";
-        display: block;
-        height: 30%;
-        position: absolute;
-        bottom: auto;
-        left: 50%;
-        right: 50%;
-        top: 50%;
-        border: 1px solid red;
-        width: 30%;
-        box-sizing: border-box;
-        z-index: -1;
-        border-radius: 50%;
-    } */
-`
+    const offset = {
+        left: referenceElement.offsetLeft,
+        top: referenceElement.offsetTop,
+        width: referenceElement.clientWidth,
+        height: referenceElement.clientHeight
+      };
+    
+      let reference = referenceElement.offsetParent;
+    
+      while (reference) {
+        offset.left += reference.offsetLeft;
+        offset.top += reference.offsetTop;
+        reference = reference.offsetParent;
+      }
+    
+      return {
+        x: position.x - offset.left,
+        y: position.y - offset.top,
+        width: offset.width,
+        height: offset.height,
+        centerX: (position.x - offset.left - offset.width / 2) / (offset.width / 2),
+        centerY: (position.y - offset.top - offset.height / 2) / (offset.height / 2)
+      };
+}
+
+function Button() {
+  const [hovered, setHovered] = React.useState(false);
+  const [mousePosition, setMousePosition] = React.useState({});
+  const boxRef = useRef();
+  const handleMouseMove = e => {
+    setMousePosition(getRelativeCoordinates(e, boxRef.current));
+  };
+
+  return (
+
+    <Wrap
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      initial={false}
+      animate={{ scale: hovered ? 1.1 : 1 }}
+    >
+
+      <motion.a
+        initial={false}
+        animate={{
+          scale: hovered ? 5.2 : 0,
+          transition: { type: "tween"},
+        }}
+      ></motion.a>
+    </Wrap>
+
+  );
+}
+
+export default Button;
+
+export const Wrap = styled(motion.button)`
+  color: white;
+  background: transparent;
+  border: 1px solid #fff;
+  border-radius: 50%;
+  height: 11rem;
+  width: 11rem;
+  cursor: pointer;
+  padding: 0;
+  z-index: 999;
+  position: relative;
+  user-select: none;
+  z-index: 1;
+  color: black;
+
+
+  a {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid white;
+    display: block;
+    margin: 0 auto;
+    background-color: #fff;
+    content: "";
+    bottom: auto;
+    width: 20%;
+    height: 20%;
+    font-size: .3rem;
+    box-sizing: border-box;
+    z-index: -1;
+    border-radius: 50%;
+  }
+
+`;
